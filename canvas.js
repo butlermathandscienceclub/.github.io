1a417c66eb16fd777182178d2184fa802408594f
butlermathandscienceclub <80225040+butlermathandscienceclub@users.noreply.github.com>
2021-03-07 17:42:20 -0500

    Create canvas.js

---
+++ canvas.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById("myCanvas");
+var ctx=canvas.getContext("2d");
+ctx.font="30px Comic Sans MS";
+ctx.fillStyle = "yellow";
+ctx.textAlign = "center";
+ctx.fillText("E=mc\u00B2", canvas.width/2, canvas.height/1.5); 
